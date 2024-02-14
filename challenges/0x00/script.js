dc86c89fce211febadfc0e2181ebba809b4c25a2
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-02-13 22:08:51 -0500

    challenge

---
+++ challenges/0x00/script.js
@@ -0,0 +1,13 @@
+const encodeKeys = {
+  e: 'enter',
+  i: 'imes',
+  a: 'ai',
+  o: 'ober',
+  u: 'ufat',
+};
+/* EXAMPLE
+gato => gaitober
+gaitober" => gato
+
+fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!
+*/
