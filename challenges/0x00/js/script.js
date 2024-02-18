d8f8f00facb134718e29fb5792a7d4111c1be308
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-02-18 15:25:25 -0500

    challenge

--- challenges/0x00/js/script.js
+++ challenges/0x00/js/script.js
@@ -1,10 +1,50 @@
-const encodeKeys = {
-  e: 'enter',
-  i: 'imes',
-  a: 'ai',
-  o: 'ober',
-  u: 'ufat',
-};
+const d = document,
+  encodeKeys = {
+    e: 'enter',
+    i: 'imes',
+    a: 'ai',
+    o: 'ober',
+    u: 'ufat',
+  },
+  re = /^[a-z]+$/;
+
+const $input = d.getElementById('input'),
+  $output = d.getElementById('output'),
+  $encode = d.getElementById('encode'),
+  $decode = d.getElementById('decode'),
+  $copy = d.getElementById('copy');
+
+d.addEventListener('DOMContentLoaded', eventsListener);
+
+function eventsListener() {
+  $input.addEventListener('change', handleInput);
+  $output.addEventListener('change', handleOutput);
+  $encode.addEventListener('click', handleEncode);
+  $decode.addEventListener('click', handleDecode);
+  $copy.addEventListener('click', handleCopy);
+}
+
+function handleInput() {
+  console.log($input.value);
+  console.log(re.test($input.value));
+}
+
+function handleOutput() {
+  console.log('handle output');
+}
+
+function handleEncode() {
+  console.log('handle encode');
+}
+
+function handleDecode() {
+  console.log('handle decode');
+}
+
+function handleCopy() {
+  console.log('handle copy');
+}
+
 /* EXAMPLE
 gato => gaitober
 gaitober" => gato
