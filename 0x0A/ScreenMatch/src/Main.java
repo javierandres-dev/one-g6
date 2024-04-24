40d9d8595dde0bf23bd07b4a445ea0d437271644
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-04-24 16:51:20 -0500

    0x0A

--- 0x0A/ScreenMatch/src/Main.java
+++ 0x0A/ScreenMatch/src/Main.java
@@ -1,7 +1,6 @@
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -9,27 +8,35 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        // final String apiUrl = "https://www.omdbapi.com/?i=tt0898266&apikey=a0858938";
-        final String apiUrl = "https://www.omdbapi.com/?t=the+big+bang+theory&apikey=a0858938";
 
-        // HttpClient
-        HttpClient client = HttpClient.newHttpClient();
-        // HttpRequest
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl))
-                .build();
-        // HttpResponse
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+  public static void main(String[] args)
+    throws IOException, InterruptedException {
+    // final String apiUrl = "https://www.omdbapi.com/?i=tt0898266&apikey=API-KEY";
+    final String apiUrl =
+      "https://www.omdbapi.com/?t=the+big+bang+theory&apikey=API-KEY";
 
-        String json = response.body();
-        System.out.println(json);
+    // HttpClient
+    HttpClient client = HttpClient.newHttpClient();
+    // HttpRequest
+    HttpRequest request = HttpRequest
+      .newBuilder()
+      .uri(URI.create(apiUrl))
+      .build();
+    // HttpResponse
+    HttpResponse<String> response = client.send(
+      request,
+      HttpResponse.BodyHandlers.ofString()
+    );
 
-        // Gson
-        //Gson gson = new Gson();
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        MyRecord myRecord = gson.fromJson(json, MyRecord.class);
-        System.out.println(myRecord);
-    }
+    String json = response.body();
+    System.out.println(json);
+
+    // Gson
+    //Gson gson = new Gson();
+    Gson gson = new GsonBuilder()
+      .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+      .create();
+    MyRecord myRecord = gson.fromJson(json, MyRecord.class);
+    System.out.println(myRecord);
+  }
 }
